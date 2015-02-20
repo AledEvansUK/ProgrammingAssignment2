058f775ec30f1ca7181b9403cd3d5c9a0f98c6b1
Aled Evans <AledEvansUK@users.noreply.github.com>
2015-02-20 12:11:55 +0000

    Update cachematrix.R
    Here's my attempt at r programming assignment 2. Aled

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL # a default setting
+	set <- function(y) { # sets the matrix value
+	x <<- y # caches the input matrix
+	m <<- NULL # sets the the matrix inverse to null
+}
+get <- function()x
+setMatInv <- function(solve) m <<- solve
+getMatInv <- function() m
+list(set=set, get=get, setMatInv = setMatInv, getMatInv=getMatInv)
 }
 
 
@@ -12,4 +16,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getMatInv()
+	if(!is.null(m)){
+	message("getting cached data")
+	return(m)
+}
+	matrix <- x$get()
+	m <- solve(matrix, ...)
+	x$setMatInv(m)
+	m
 }
